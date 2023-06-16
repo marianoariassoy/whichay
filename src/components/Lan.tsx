433a6d289737d45d7218807796c746e4d496fd1f
marianoariassoy <hola@marianoarias.soy>
2023-06-16 15:01:46 -0300

    lan

--- src/components/Lan.tsx
+++ src/components/Lan.tsx
@@ -6,14 +6,14 @@ const Lan = () => {
   return (
     <ul className="flex gap-1 items-center text-sm">
       <li>
-        <button className={`${lan === "es" && "opacity-50"}`} onClick={() => setLan("en")}>
-          EN
+        <button className={` ${lan === "es" ? "opacity-50" : "hover:text-black"}`} onClick={() => setLan("es")}>
+          ES
         </button>
       </li>
       <li> - </li>
       <li>
-        <button className={`${lan === "en" && "opacity-50"}`} onClick={() => setLan("es")}>
-          ES
+        <button className={` ${lan === "en" ? "opacity-50" : "hover:text-black"}`} onClick={() => setLan("en")}>
+          EN
         </button>
       </li>
     </ul>
