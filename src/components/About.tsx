72b97df0abaf77167079c14beae903df25183a07
marianoariassoy <hola@marianoarias.soy>
2023-06-23 13:12:15 -0300

    add english text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,27 +9,49 @@ const About = () => {
     changeColor("#about");
   }, []);
 
+  const text = {
+    es: {
+      title: "Somos",
+      subtitle: "Construimos porque nos apasiona el poder transformador de nuestra industria.",
+      text1: "Nos motiva experimentar cómo una obra, sin importar su tamaño, incentiva el desarrollo de todo su entorno, generando crecimiento y evolución social.",
+      text2: "Somos una empresa jujeña dedicada al desarrollo y construcción de obras civiles dentro de los sectores público y privado.",
+      text3:
+        "Contamos con el respaldo técnico de un excepcional equipo de trabajo y el networking necesario para el óptimo desarrollo y construcción de obras de arquitectura e ingeniería de gran escala en todo el territorio provincial, enfocados continuamente en satisfacer los requerimientos y las expectativas de nuestros clientes.",
+      text4: "CONSTRUIMOS FUTURO",
+      text5: "Miramos hacia adelante, buscando siempre superarnos sobre la base de lo que hemos transcurrido.",
+    },
+    en: {
+      title: "We Are",
+      subtitle: "We build because we are passionate about the transforming power of our industry.",
+      text1: "We are motivated to experience how a work, regardless of its size, encourages the development of its entire environment, generating growth and social evolution.",
+      text2: "We are a Jujuy company dedicated to the development and construction of civil works within the public and private sectors.",
+      text3:
+        "We have the technical support of an exceptional team and the necessary networking for the optimal development and construction of large-scale architectural and engineering works throughout the provincial territory, continuously focused on satisfying the requirements and expectations of our clients.",
+      text4: "WE BUILD FUTURE",
+      text5: "We look forward, always seeking to improve on the basis of what we have gone through.",
+    },
+  };
+
   return (
     <section id="about">
       <div className="container px-14 py-32 md:flex gap-8 items-center m-auto max-w-7xl">
         <div className="md:w-1/3">
           <h1 className="text-5xl md:text-7xl my-4">
-            <span className="font-light block">{lan === "es" ? "Somos" : "We Are"}</span>
+            <span className="font-light block">{text[lan].title}</span>
             <span className="font-bold block text-primary">WICHAY</span>
           </h1>
         </div>
         <div className="md:w-2/3">
-          <p className="text-xl mb-4 text-primary font-bold italic">Construimos porque nos apasiona el poder transformador de nuestra industria.</p>
-          <p className="mb-8 text-wrap-balance">
-            Nos motiva experimentar cómo una obra, sin importar su tamaño, incentiva el desarrollo de todo su entorno, generando crecimiento y evolución social. <br />
-            <br />
-            Somos una empresa jujeña dedicada al desarrollo y construcción de obras civiles dentro de los sectores público y privado.
-            <br />
-            Contamos con el respaldo técnico de un excepcional equipo de trabajo y el networking necesario para el óptimo desarrollo y construcción de obras de arquitectura e ingeniería de gran escala en todo el territorio provincial, enfocados
-            continuamente en satisfacer los requerimientos y las expectativas de nuestros clientes.
-          </p>
-          <p className="text-xl mb-4 text-primary font-bold italic">CONSTRUIMOS FUTURO</p>
-          <p className="font-medium italic">“Miramos hacia adelante, buscando siempre superarnos sobre la base de lo que hemos transcurrido”.</p>
+          <h3 className="text-xl mb-4 text-primary font-bold italic">{text[lan].subtitle}</h3>
+          <div className="mb-8">
+            <p className="text-wrap-balance mb-4">
+              {text[lan].text1} <br />
+              {text[lan].text2}
+            </p>
+            <p className="text-wrap-balance">{text[lan].text3}</p>
+          </div>
+          <h3 className="text-xl mb-4 text-primary font-bold italic">{text[lan].text4}</h3>
+          <p className="font-medium italic">{text[lan].text5}</p>
         </div>
       </div>
     </section>
--- src/components/Sustentability.tsx
+++ src/components/Sustentability.tsx
@@ -10,18 +10,32 @@ const Sustentability = () => {
     changeColor("#sustentability");
   }, []);
 
+  const text = {
+    es: {
+      title: "Sustentabilidad",
+      text1: "Aceptamos el desafío que hoy se impone en nuestro sector para ser parte de la construcción de un futuro sostenible.",
+      text2:
+        "Acompañamos el compromiso de nuestra provincia con el cuidado del medio ambiente, por ello capacitamos a nuestros técnicos y operarios en el desarrollo de nuevas técnicas constructivas que tiendan al aprovechamiento renovable de energías.",
+      text3: "Una de las formas de aplicar estas energías renovables es a partir de la energía fotovoltaica y solar térmica. En ese marco ejecutamos instalaciones de PANELES y TERMOTANQUES SOLARES",
+    },
+    en: {
+      title: "Sustentability",
+      text1: "We accept the challenge that is imposed on our sector today to be part of the construction of a sustainable future.",
+      text2: "We accompany the commitment of our province to the care of the environment, for this reason we train our technicians and operators in the development of new construction techniques that tend to the renewable use of energy.",
+      text3: "One of the ways to apply these renewable energies is from photovoltaic and solar thermal energy. In this framework, we carry out installations of SOLAR PANELS and SOLAR WATER HEATERS",
+    },
+  };
+
   return (
     <section id="sustentability">
       <div className="container px-14 pb-24 pt-32 md:flex gap-8 items-center m-auto max-w-7xl">
         <div className="md:w-2/3 md:pr-16">
-          <h1 className="text-4xl md:text-6xl font-bold text-primary mb-8">{lan === "es" ? "Sustentabilidad" : "Sustentability"}</h1>
-          <p className="mb-8 text-wrap-balance">
-            Aceptamos el desafío que hoy se impone en nuestro sector para ser parte de la construcción de un futuro sostenible.
-            <br /> <br />
-            Acompañamos el compromiso de nuestra provincia con el cuidado del medio ambiente, por ello capacitamos a nuestros técnicos y operarios en el desarrollo de nuevas técnicas constructivas que tiendan al aprovechamiento renovable de energías.
-            <br /> <br />
-            Una de las formas de aplicar estas energías renovables es a partir de la energía fotovoltaica y solar térmica. En ese marco ejecutamos instalaciones de PANELES y TERMOTANQUES SOLARES
-          </p>
+          <h1 className="text-4xl md:text-6xl font-bold text-primary mb-8">{text[lan].title}</h1>
+          <div className="mb-8 ">
+            <p className="text-wrap-balance mb-4">{text[lan].text1}</p>
+            <p className="text-wrap-balance mb-4">{text[lan].text2}</p>
+            <p className="text-wrap-balance">{text[lan].text3}</p>
+          </div>
         </div>
         <div className="md:w-1/3">
           <ImageComponent src="/images/sustentability.jpg" alt="Sustentability" />
